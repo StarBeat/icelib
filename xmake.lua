f2149a1d664d0376271a0cf0525f1c6210b79eaf
StarBeat <977663818@qq.com>
2023-09-10 20:18:47 +0800

    xrepo info stunlib linkdir 疑似有bug

--- xmake.lua
+++ xmake.lua
@@ -19,7 +19,7 @@ target("icelib")
     
     add_packages("xnet")
     add_packages("stunlib")
-    
+   
     add_includedirs("include")
     add_headerfiles("include/**.h")
     add_files("src/fifo.c",
@@ -32,13 +32,32 @@ target("icelib.test.icelib")
     
     set_group("icelib.test")
     add_deps("icelib")
+    add_packages("stunlib", {links = "stunlib"})
     add_packages("xnet")
-    add_packages("stunlib")
-    
     add_includedirs("test")
     add_includedirs("include")
     add_files("test/test_utils.c")
     add_files("test/icelib_test.c")
+
+
+    on_load(function (target)
+        print("-------")
+        local zlib = target:pkg("stunlib")
+        if zlib then
+            print(zlib:get("links"), zlib:get("includedirs"))
+        end
+            -- import("lib.detect.find_package")
+        -- local outdata, errdata = os.iorun("xrepo info xnet")
+        -- print(outdata)
+        -- local s, e = string.find(outdata, "linkdirs.")
+        -- print(s)
+        -- print(e)
+        -- local dir = string.sub(outdata, e + 1)
+        -- dir = string.gsub(dir, " ", "")
+        -- local linkdir = string.gsub(dir, "\n", "")
+        -- print(linkdir)
+        print("-------")
+    end)
 target_end()
 
 target("icelib.test.icelibtypes")
